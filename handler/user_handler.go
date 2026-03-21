c1b9d7781a82542de9736d0ee4e9f5efe7e71b13
Pitiphat Jadnuang <woonwai@Pitiphats-MacBook-Air.local>
2026-03-21 15:20:22 +0700

    user_handler

--- handler/user_handler.go
+++ handler/user_handler.go
@@ -16,23 +16,7 @@ func NewUserHandler(service service.UserService) *UserHandler {
 	return &UserHandler{service: service}
 }
 
-func (h *UserHandler) GetUserByID(c *gin.Context) {
-	user, err := h.service.GetUserByID(c.Param("userId"))
-	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, user)
-}
-
-func (h *UserHandler) GetDriverByID(c *gin.Context) {
-	driver, err := h.service.GetDriverByID(c.Param("userId"))
-	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, driver)
-}
+//viewpayment
 
 func (h *UserHandler) ViewPaymentHistory(c *gin.Context) {
 	payments, err := h.service.ViewPaymentHistory(c.Param("userId"))
@@ -41,13 +25,4 @@ func (h *UserHandler) ViewPaymentHistory(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, payments)
-}
-
-func (h *UserHandler) ViewUsageHistory(c *gin.Context) {
-	sessions, err := h.service.ViewUsageHistory(c.Param("userId"))
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, sessions)
 }
